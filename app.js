fe3d637abeafb1c6ac959ec046c8a2431804a422
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-11-14 19:25:33 +0000

    Added - Data in XML file

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ var http = require('http'),
     xmlParse = require('xslt-processor').xmlParse,
     // XML PARSE PROCESSOR
     xsltProcess = require('xslt-processor').xsltProcess;
+
+// CREATING SERVER
+var router = express();
+var server = http.createServer(router);
+
+router.get('/', function(req, res) {
+
+    res.writeHead(200, {'Content-Type':'text/html'});
+
+    // READING FILES - FILE SYSTEM
+    var xml = fs.readFileSync('StockInventory.xml', 'utf8');
+    var xsl = fs.readFileSync('StockInventory.xsl', 'utf8');
+
+    // PARSING XML AND XSL
+    var doc = xmlParse(xml);
+    var stylesheet = xmlParse(xsl);
+
+    // EXECUTING
+    var result = xsltProcess(doc, stylesheet);
+
+    // SERVING THE RESULT (HTML)
+    res.end(result.toString());
+
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP, function(){
+    var addr = server.address();
+    console.log('Server is listening to ', addr.address + ':' + addr.port );
+});
