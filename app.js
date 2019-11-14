9ee7c7e9e8ea46822b9161e7c4e149b1ba85bee1
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-11-14 18:11:45 +0000

    Added Http, Express, Fs and Xslt-processor modules and dependencies

---
+++ app.js
@@ -0,0 +1,10 @@
+    // REQUEST 
+var http = require('http'),
+    // ROUTING
+    express = require('express'),
+    // FILE SYSTEM
+    fs = require('fs'),
+    // XML PARSE
+    xmlParse = require('xslt-processor').xmlParse,
+    // XML PARSE PROCESSOR
+    xsltProcess = require('xslt-processor').xsltProcess;
