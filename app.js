17003a55a2008ad530f10b7fc5611b25f4777c31
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-11-30 11:32:06 +0000

    Added functions and POST request method when adding new Stock Items

--- app.js
+++ app.js
@@ -10,12 +10,33 @@ var http = require('http'),
     xmlParse = require('xslt-processor').xmlParse,
     // XML PARSE PROCESSOR
     xsltProcess = require('xslt-processor').xsltProcess;
+    // XML TO JSON MODULE
+    xml2js = require('xml2js');
 
 // CREATING SERVER
 var router = express();
 var server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname, 'views')));
+router.use(express.urlencoded({extended: true}));
+router.use(express.json());
+
+// Function to read in XML file and convert it to JSON
+function xmlFileToJs(filename, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+    if (err) throw (err);
+    xml2js.parseString(xmlStr, {}, cb);
+  });
+}
+
+// Function to convert JSON to XML and save it
+function jsToXmlFile(filename, obj, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  var builder = new xml2js.Builder();
+  var xml = builder.buildObject(obj);
+  fs.writeFile(filepath, xml, cb);
+}
 
 router.get('/', function(req, res) {
     res.render('index');
@@ -38,6 +59,31 @@ router.get('/get/stockitems', function(req, res) {
 
     // SERVING THE RESULT (HTML)
     res.end(result.toString());
+
+});
+
+// POST request to add to JSON & XML files
+router.post('/post/json', function(req, res) {
+
+  // Function to read in a JSON file, add to it & convert to XML
+  function appendJSON(obj) {
+    console.log(obj);
+    // Function to read in XML file, convert it to JSON, add a new object and write back to XML file
+    xmlFileToJs('StockInventory.xml', function(err, result) {
+      if (err) throw (err);
+      result.cafemenu.section[obj.sec_n].entree.push({'name': obj.name, 'description': obj.description});
+      console.log(result);
+      jsToXmlFile('StockInventory.xml', result, function(err) {
+        if (err) console.log(err);
+      })
+    })
+  }
+
+  // Call appendJSON function and pass in body of the current POST request
+  appendJSON(req.body);
+
+  // Re-direct the browser back to the page, where the POST request came from
+  res.redirect('back');
 
 });
 
