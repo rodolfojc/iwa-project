d5169100b298cf71bf34036154de7f00c4f302cd
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-12-02 15:33:31 +0000

    DELETE stock item - working#

--- views/js/table.js
+++ views/js/table.js
@@ -22,10 +22,29 @@ function select_row()
 		$(this).addClass("selected");
 		var section = $(this).prevAll("tr").children("td[colspan='6']").length - 1;
         var item = $(this).attr("id") - 1;
-        alert("Section: "+ section + " Item: " + item);
-		//delete_row(section, entree);
+        delete_row(section, item);
 	})
 };
+
+function delete_row(sec, item)
+{
+	$("#delete").click(function ()
+	{
+		$.ajax(
+		{
+			url: "/post/delete",
+			type: "POST",
+			data:
+			{
+				section: sec,
+				item: item,
+			},
+			cache: false,
+			success: setTimeout(draw_table, 1000)
+		})
+	})
+};
+
 
 $(document).ready(function(){
     draw_table();
