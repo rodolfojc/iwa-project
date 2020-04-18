adcc6350706915a7132a37132513c0843f5854f2
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-04-18 12:34:48 +0000

    Typo error - Fixed

--- routes/itemsRoutes.js
+++ routes/itemsRoutes.js
@@ -2,10 +2,10 @@ var express = require('express'),
     router = express.Router();
     itemController = require('../controllers/itemController');
 
-router.post('/iteam', itemController.createItem);
-router.get('/iteam', itemController.getItem);
-router.get('/iteam/:id', itemController.getItem);
-router.delete('/iteam/:id', itemController.deleteItem);
-router.put('/iteam/:id', itemController.updateItem);
+router.post('/item', itemController.createItem);
+router.get('/item', itemController.getItem);
+router.get('/item/:id', itemController.getItem);
+router.delete('/item/:id', itemController.deleteItem);
+router.put('/item/:id', itemController.updateItem);
 
 module.exports = router;
