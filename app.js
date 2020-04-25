60b1f52e26e139c00a8c0cc32338457e57e229a7
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-04-25 13:45:31 +0000

    More security modules added

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ var logger = require("morgan"),
     bodyParser = require("body-parser"),
     mongoose = require('mongoose'),
     path = require('path');
+    
+   
 
 // ENV VARIABLES
 require('dotenv').config();
 
 // SANATAIZER
-const expAutoSan = require('express-autosanitizer');
+var expAutoSan = require('express-autosanitizer');
+
+//INPUT CLEANER FOR XSS ATTACKS
+var xss = require('xss-clean');
+
+// HELMET
+var helmet = require('helmet');
+
+// MONGOOSE SANITIZATION AGAIST NOSLQ INJECTION ATTACKS
+mongoSanitize = require('express-mongo-sanitize');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -26,6 +37,15 @@ app.use(express.static(path.resolve(__dirname, 'views')));
 // MOUNT SANATAIZER
 app.use(expAutoSan.all);
 
+// MOUNT XSS CLEAN
+app.use(xss());
+
+// MOUNT HELMET
+app.use(helmet());
+
+// MOUNT MONGOOSE SANITIZER
+app.use(mongoSanitize());
+
 // MAIN CALL
 app.get('/', (req, res) => {
     res.render('index');
@@ -36,12 +56,14 @@ app.listen(port, function(err){
     console.log("Listening on Port: " + port)
 });
 
-// MONGOOSE CONNECTION
+// MONGOOSE CONNECTION / ERROR HANDLER
 mongoose.connect(process.env.MONGODB_URL);
 mongoose.connection.on('error', (err) => { 
     console.log('Mongodb Error: ', err); 
     process.exit();
 });
+
+// CONNECTION NOTIFICATION
 mongoose.connection.on('connected', () => { 
     console.log('MongoDB is successfully connected');
 });
